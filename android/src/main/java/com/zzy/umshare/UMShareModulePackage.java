52a030cc66310e68cbb87c6d46b22bf689865532
zhangzy <zhangzy0816@gmail.com>
2018-04-26 10:30:03 +0800

    android 兼容rn4.3+

--- android/src/main/java/com/zzy/umshare/UMShareModulePackage.java
+++ android/src/main/java/com/zzy/umshare/UMShareModulePackage.java
@@ -30,7 +30,6 @@ public class UMShareModulePackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
